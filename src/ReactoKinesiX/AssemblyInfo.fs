6d701d720d74032af4f8f29b68697828e1461480
theburningmonk@gmail.com <theburningmonk@gmail.com>
2014-03-10 16:48:51 +0000

    - added package for beta release of 0.2.0

--- src/ReactoKinesiX/AssemblyInfo.fs
+++ src/ReactoKinesiX/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("ReactoKinesix")>]
 [<assembly: AssemblyProductAttribute("ReactoKinesix")>]
 [<assembly: AssemblyDescriptionAttribute("A Rx-based .Net client library for Amazon Kinesis")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 [<assembly: InternalsVisibleToAttribute("ReactoKinesiX.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] Version = "0.2.0"
